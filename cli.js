7604d0cf2e680d4d55c4308512824c8f83afee25
Marko Klemetti <marko.klemetti@gmail.com>
2023-04-10 09:44:34 +0300

    chore(app): rename cli name

--- cli.js
+++ cli.js
@@ -5,8 +5,8 @@ const yargs = require('yargs');
 const weeknumber = require('./src/weeknumber');
 
 yargs(process.argv.slice(2))
-  .usage('Usage: npx @mrako/week-number [options]')
-  .example('npx @mrako/week-number', 'show the current week number')
+  .usage('Usage: npx @hopefullyworks/week-number [options]')
+  .example('npx @hopefullyworks/week-number', 'show the current week number')
   .help('h')
   .alias('h', 'help')
   .alias('v', 'version')
